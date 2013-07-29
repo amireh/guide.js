f995fdd5db3021a5bc4315820f67e0507c90ed79
Ahmad Amireh <ahmad.amireh@gmail.com>
2013-07-30 01:07:57 +0300

    a try-out spec for ext/lodash using Jasmine and a Grunt runner

---
+++ spec/unit/lodash.js
@@ -0,0 +1,59 @@
+describe("lodash", function() {
+  it("_.assign()", function() {
+    var o = {};
+
+    expect(_.assign('foo.bar', 123, o)).toEqual({
+      foo: { bar: 123 }
+    });
+
+    expect(_.assign('foo.bar', null, o)).toEqual({
+      foo: { bar: null }
+    });
+  });
+
+  it("_.assign() should implicitly create an object", function() {
+    expect(_.assign('foo.bar', 123, undefined)).toEqual({
+      foo: { bar: 123 }
+    });
+  });
+
+  describe("_.parseOptions", function() {
+
+    it("should parse options", function() {
+      expect(_.parseOptions('foo:bar')).toEqual({
+        foo: 'bar'
+      });
+    });
+
+    it("should parse multiple options", function() {
+      expect(_.parseOptions('a:1, b:2, c:false')).toEqual({
+        a: 1,
+        b: 2,
+        c: false
+      });
+    });
+
+    it("should omit non-significant whitespace", function() {
+      expect(_.parseOptions('a: 1 , b: 2')).toEqual({
+        a: 1,
+        b: 2
+      });
+    });
+
+    it("should convert option values", function() {
+      expect(_.parseOptions('foo:123')).toEqual({ foo: 123 })
+      expect(_.parseOptions('foo:123')).not.toEqual({ foo: '123' })
+
+      expect(_.parseOptions('foo:true')).toEqual({ foo: true })
+      expect(_.parseOptions('foo:false')).toEqual({ foo: false })
+    });
+
+    it("should use a custom tokenizer", function() {
+    });
+
+    it("should use a custom sanitizer", function() {
+    });
+
+  });
+
+})
