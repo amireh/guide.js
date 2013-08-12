7c06e3740476b499e7f36000b7e99f90d5374a53
Ahmad Amireh <ahmad.amireh@gmail.com>
2013-08-13 01:59:13 +0300

    Release 1.3.0

--- dist/guide.js
+++ dist/guide.js
@@ -793,7 +793,7 @@
   }); // guide.prototype
 
   Guide = new Guide();
-  Guide.VERSION = '1.2.7';
+  Guide.VERSION = '1.3.0';
 
   // expose the instance to everybody
   if (typeof exports !== 'undefined') {
